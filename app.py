d650adf4ff226d6187b3c60f834fa88668ec68c2
weijchen <weijchen0919@gmail.com>
2017-12-21 00:50:50 +0800

    Ver: 1.1 (building enter modules)

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 '''
 Author: Jimmy Chen
-PN: Billing Tracking, Created Dec. 2017
-Ver: 1.0 (project start)
+PN: Billing Tracker, Created Dec. 2017
+Ver: 1.1 (building enter modules)
 Link:
 Todo: 
 ''' 
@@ -14,53 +14,308 @@ from datetime import timedelta
 import pandas as pd
 # --------------------------------------------------- init settings
 # --------------------------------------------------- Functions
-class Enter(object):
+class Exp(object):
+    """docstring for Exp"""
+    def __init__(self, arg):
+        super(Exp, self).__init__()
+        self.arg = arg
+    def enter():
+        deal = []
+        Show.account()
+        check = str(input("To account: "))
+        while (Enter.account_check(int(check)) == False):
+            print("Title not exist!")
+            check = str(input("To account(Q to leave): "))
+            if check == 'Q' or 'q':
+                return 0
+        else:
+            account = Enter.account_check(check)
+
+        check = str(input("Today? (Y/N): "))
+        date_, time_ = Enter.get_time(check)
+        # print(type(Enter.get_id(account, date_)))
+
+        id_ = Enter.get_id(account, date_)
+        # print(id_)
+
+        amount = int(input("Amount: "))
+        main = Enter.get_main()
+        sub = Enter.get_sub(main)
+        details = str(input("Details: "))
+        if len(details) == 0:
+            details = "None"
+        invoice = str(input("Invoice: "))
+        if len(invoice) == 0:
+            invoice = "None"
+        
+        # date = [_ for _ in datetime.fromtimestamp(js['t'][idx]).strftime("%Y,%m,%d,%H:%M:%S,%a").split(',')]
+        # year, month, day, time, weekday = date[0], date[1], date[2], date[3], date[4]
+        # id_ = str(ticker)+year+month+day
+        # print(id_, year, month, day, time, weekday, volumn, open_, high, low, close, change)
+        sqlstr_inc_ent = ("INSERT OR IGNORE INTO income (id, date, time, amount, account, main, sub, details, invoice) VALUES (?,?,?,?,?,?,?,?,?)")
+        deal.append((id_, date_, time_, amount, account, main, sub, details, invoice))
+        conn.executemany(sqlstr_inc_ent, deal)
+        conn.commit()
+
+        # Count the number of records:
+        # SELECT COUNT(*) FROM your_table_name
+    def get_time(check):
+        if check == 'Y' or check == 'y':
+            out_1, out_2 = str(datetime.datetime.now()).split(" ")[0], str(datetime.datetime.now()).split(" ")[1]
+        else:
+            year = str(input("Which year? ")) 
+            month = str(input("Which month? ")) 
+            day = str(input("Which day? "))
+            time = str(datetime.datetime.now()).split(" ")[1]
+
+            out_1 = year+"-"+month+"-"+day
+            out_2 = time
+        return out_1, out_2
+    def get_id(account, date):
+        cur = conn.cursor()
+        sqlstr = "SELECT Count(*) FROM income"
+        cur.execute(sqlstr)
+        var = cur.fetchone()
+        print(var)
+
+        return var[0]+1
+    def get_main():
+        cat = ['Food', 'Trafic']
+
+        # Display cat
+        for idx in range(len(cat)):
+            print('{}.{} '.format(idx+1, cat[idx]), end=' ')
+        print("")
+
+        choose = int(input("Which category? "))
+        while choose < 0 and choose > len(cat):
+            choose = int(input("Which category? "))
+        else:
+            return cat[choose-1]
+    def get_sub(main):
+        dic = {
+            'Food': {
+                '1': 'Breakfast',
+                '2': 'Lunch',
+                '3': 'Dinner'
+            },
+            'Trafic':{
+                '1': 'Motorcycle',
+                '2': 'Gasoline'
+            }
+        }
+        for key, val in enumerate(dic[main]):
+            print('{}.{} '.format(key+1, dic[main][val]), end=' ')
+
+        choose = str(input("Which item? "))
+        # print(len(dic[main]))
+        while int(choose) < 0 and int(choose)-1 == len(dic[main]):
+            choose = str(input("Which category? "))
+        return dic[main][choose]
+
+class Acc(object):
+    """docstring for Acc"""
+    def __init__(self, arg):
+        super(Acc, self).__init__()
+        self.arg = arg
+    def enter(check):
+        try:
+            sqlstr = "SELECT title FROM account WHERE title = {}".format(check)
+            n = conn.execute(sqlstr)
+        except Exception as e:
+            print(e)
+        def account():
+        accs = []
+        id_ = Enter.get_account_length()+1
+        date_, time_ = str(datetime.datetime.now()).split(" ")[0], str(datetime.datetime.now()).split(" ")[1]
+        title = str(input("Enter account name: "))
+        while (Enter.account_check(title) == True):
+            print("Title already exist!")
+            title = str(input("Enter account name: "))
+        amount = int(input("Init value: "))
+        details = str(input("Details: "))
+        if len(details) == 0:
+            details = "None"
+
+        accs.append((id_, date_, time_, title, amount, details))
+        # print(accs)
+
+        sqlstr_acc_ent = ("INSERT OR IGNORE INTO account (id, date, time, title, amount, details) VALUES (?,?,?,?,?,?)")
+        conn.executemany(sqlstr_acc_ent, accs)
+        conn.commit()
+    def account_check(check):
+        '''check the existence of particular account'''
+        if type(check) == int:
+            '''income() by int to choose account'''
+            cur = conn.cursor()
+            sqlstr = "SELECT title FROM account WHERE id = '{}';".format(check)
+            cur.execute(sqlstr)
+            try:
+                var = cur.fetchone()
+                return var[0]
+            except:
+                return False
+        if type(check) == str:
+            '''account() by string'''
+            cur = conn.cursor()
+            sqlstr = "SELECT title FROM account WHERE title = '{}';".format(check)
+            cur.execute(sqlstr)
+            try:
+                var = cur.fetchone()
+                return (len(var) >= 1)
+            except:
+                return False
+    def get_account_length():
+        cur = conn.cursor()
+        sqlstr = "SELECT Count(*) FROM account;"
+        cur.execute(sqlstr)
+        var = cur.fetchone()
+        return var[0]
+
+class Inc(object):
     """docstring for Enter"""
     def __init__(self, arg):
         super(Enter, self).__init__()
         self.arg = arg
-    def income():
-        # display all account
-        check = int(input("To account: "))
-        account = get_account(check)
-        # check accessability of this account
-        check = str(input("Today? (Y/N)"))
-        date_ = self.get_time(check)
-        id_ = date_+account+
+    def enter():
+        deal = []
+        Show.account()
+        check = str(input("To account: "))
+        while (Enter.account_check(int(check)) == False):
+            print("Title not exist!")
+            check = str(input("To account(Q to leave): "))
+            if check == 'Q' or 'q':
+                return 0
+        else:
+            account = Enter.account_check(check)
+
+        check = str(input("Today? (Y/N): "))
+        date_, time_ = Enter.get_time(check)
+        # print(type(Enter.get_id(account, date_)))
+
+        id_ = Enter.get_id(account, date_)
+        # print(id_)
+
         amount = int(input("Amount: "))
-        primary = self.get_primary()
-        secondary = self.get_secondary()
+        main = Enter.get_main()
+        sub = Enter.get_sub(main)
         details = str(input("Details: "))
+        if len(details) == 0:
+            details = "None"
         invoice = str(input("Invoice: "))
+        if len(invoice) == 0:
+            invoice = "None"
+        
+        # date = [_ for _ in datetime.fromtimestamp(js['t'][idx]).strftime("%Y,%m,%d,%H:%M:%S,%a").split(',')]
+        # year, month, day, time, weekday = date[0], date[1], date[2], date[3], date[4]
+        # id_ = str(ticker)+year+month+day
+        # print(id_, year, month, day, time, weekday, volumn, open_, high, low, close, change)
+        sqlstr_inc_ent = ("INSERT OR IGNORE INTO income (id, date, time, amount, account, main, sub, details, invoice) VALUES (?,?,?,?,?,?,?,?,?)")
+        deal.append((id_, date_, time_, amount, account, main, sub, details, invoice))
+        conn.executemany(sqlstr_inc_ent, deal)
+        conn.commit()
 
-
-        sqlstr = "SELECT TABLE income ()"
-        sqlstr_inc_ent = ("INSERT OR IGNORE INTO income (id, date_, amount, account, primary, secondary, details, invoice) VALUES (?,?,?,?,?,?,?)")
-
-    def get_account(check):
-        try:
-            sqlstr = "SELECT TABLE account ()"
-        except Exception as e:
-            print(e)
-
+        # Count the number of records:
+        # SELECT COUNT(*) FROM your_table_name
     def get_time(check):
         if check == 'Y' or check == 'y':
-            out = str(datetime.datetime.now())
+            out_1, out_2 = str(datetime.datetime.now()).split(" ")[0], str(datetime.datetime.now()).split(" ")[1]
         else:
-            out = str(input("Which day? "))
-        return out
+            year = str(input("Which year? ")) 
+            month = str(input("Which month? ")) 
+            day = str(input("Which day? "))
+            time = str(datetime.datetime.now()).split(" ")[1]
 
-    def get_primary():
-        pass
-    def get_secondary():
-        pass
+            out_1 = year+"-"+month+"-"+day
+            out_2 = time
+        return out_1, out_2
+    def get_id(account, date):
+        cur = conn.cursor()
+        sqlstr = "SELECT Count(*) FROM income"
+        cur.execute(sqlstr)
+        var = cur.fetchone()
+        print(var)
 
+        return var[0]+1
+    def get_main():
+        cat = ['Food', 'Trafic']
 
+        # Display cat
+        for idx in range(len(cat)):
+            print('{}.{} '.format(idx+1, cat[idx]), end=' ')
+        print("")
+
+        choose = int(input("Which category? "))
+        while choose < 0 and choose > len(cat):
+            choose = int(input("Which category? "))
+        else:
+            return cat[choose-1]
+    def get_sub(main):
+        dic = {
+            'Food': {
+                '1': 'Breakfast',
+                '2': 'Lunch',
+                '3': 'Dinner'
+            },
+            'Trafic':{
+                '1': 'Motorcycle',
+                '2': 'Gasoline'
+            }
+        }
+        for key, val in enumerate(dic[main]):
+            print('{}.{} '.format(key+1, dic[main][val]), end=' ')
+
+        choose = str(input("Which item? "))
+        # print(len(dic[main]))
+        while int(choose) < 0 and int(choose)-1 == len(dic[main]):
+            choose = str(input("Which category? "))
+        return dic[main][choose]
+
+class Show(object):
+    """docstring for Show"""
+    def __init__(self, arg):
+        super(Show, self).__init__()
+        self.arg = arg
+    
+    def account():
+        cur = conn.cursor()
+        sqlstr = "SELECT title FROM account;"
+        cur.execute(sqlstr)
+        # try:
+        var = cur.fetchall()
+        if len(var) == 0:
+            print("No account created!")
+        else:
+            for idx in range(len(var)):
+                print('{}.{} '.format(idx+1, var[idx][0]), end=' ')
+            print("")
+    def income():
+        cur = conn.cursor()
+        sqlstr = ("SELECT * FROM income ORDER BY date ASC, time ASC;")
+        cur.execute(sqlstr)
+        # try:
+        var = cur.fetchall()
+        if len(var) == 0:
+            print("No account created!")
+        else:
+            for idx in range(len(var)):
+                print(var[idx])
+            print("")
+
+class Modify(object):
+    """docstring for Modify"""
+    def __init__(self, arg):
+        super(Modify, self).__init__()
+        self.arg = arg
+    def income():
+        cur = conn.cursor()
+        
 class Display(object):
     """docstring for Display"""
     def __init__(self, arg):
         super(Display, self).__init__()
         self.arg = arg
+
     def main():
         print("-------------------")
         print("Save some Money!")
@@ -83,15 +338,26 @@ class Display(object):
         print("0. End")
         print("-------------------")
 
-    # def modify():
-    #     print("-------------------")
-    #     print("Modify data")
-    #     print("-------------------")
-    #     print("1. Enter income")
-    #     print("2. Enter expense")
-    #     print("3. Enter account")
-    #     print("0. End")
-    #     print("-------------------")
+    def modify():
+        print("-------------------")
+        print("Modify data")
+        print("-------------------")
+        print("1. Modify income")
+        print("2. Modify expense")
+        print("3. Modify account")
+        print("0. End")
+        print("-------------------")
+
+    def show():
+        print("-------------------")
+        print("Show data")
+        print("-------------------")
+        print("1. Show income")
+        print("2. Show expense")
+        print("3. Show account")
+        print("0. End")
+        print("-------------------")
+
 # --------------------------------------------------- Control center
 if __name__ == '__main__':
     while True:
@@ -105,19 +371,19 @@ if __name__ == '__main__':
         # -- 1. Create table --
         elif choice == 1:
             try:
-                sqlstr_exp = 'CREATE TABLE expense (id TEXT UNIQUE, date_ TEXT, amount NUMERIC, account TEXT, primary TEXT, secondary TEXT, details TEXT, invoice TEXT)'
+                sqlstr_exp = "CREATE TABLE expense (id TEXT UNIQUE, date TEXT, time DATETIME, amount NUMERIC, account TEXT, main TEXT, sub TEXT, details TEXT, invoice TEXT)"
                 conn.execute(sqlstr_exp)
                 conn.commit()
             except Exception as e:
                 print(e)
             try:
-                sqlstr_inc = 'CREATE TABLE income (id TEXT UNIQUE, date_ TEXT, amount NUMERIC, account TEXT, primary TEXT, secondary TEXT, details TEXT, invoice TEXT)'
+                sqlstr_inc = 'CREATE TABLE income (id TEXT UNIQUE, date DATETIME, time DATETIME, amount NUMERIC, account TEXT, main TEXT, sub TEXT, details TEXT, invoice TEXT)'
                 conn.execute(sqlstr_inc)
                 conn.commit()
             except Exception as e:
                 print(e)
             try:
-                sqlstr_acc = 'CREATE TABLE account (id TEXT UNIQUE, date_ TEXT, title TEXT, amount NUMERIC, details TEXT)'
+                sqlstr_acc = 'CREATE TABLE account (id TEXT UNIQUE, date DATETIME, time DATETIME, title TEXT, amount NUMERIC, details TEXT)'
                 conn.execute(sqlstr_acc)
                 conn.commit()
             except Exception as e:
@@ -134,66 +400,45 @@ if __name__ == '__main__':
                 break
             # -- 2-1. Enter income --
             elif choice == 1:
-                Enter.income()
-
-                print("-- Crawl Comp: {} --".format(tickr))
-                try:
-                    crawler(tickr)
-                except Exception as e:
-                    print(e)
-                conn.close()
+                Inc.enter()
+                # conn.close()
             # -- 2-2. Enter expense --
             elif choice == 2:
-                for row in querylis:
-                    time.sleep(0.5)
-                    tickr, comp_name = row[0], row[1]
-                    conn = sqlite3.connect('db/{}.sqlite'.format(tickr))
-                    print("-- Crawl Comp: {} --".format(tickr))
-                    try:
-                        crawler(tickr)
-                    except Exception as e:
-                        print(e)
-                    conn.close()
+                Exp.enter()
             # -- 2-3. Enter account --
+            elif choice == 3:
+                Acc.enter()
+        # -- 3. Modify data --
+        elif choice == 3:
+            Display.modify()
+            choice = int(input("Choose function: "))
+            print("-----------------------------------------")
+            # -- 4-0. Exit program --
+            if choice == 0:
+                break
+            # -- 4-1. Show income --
+            elif choice == 1:
+                Modify.income()
+            # -- 4-2. Show expense --
             elif choice == 2:
-                for row in querylis:
-                    time.sleep(0.5)
-                    tickr, comp_name = row[0], row[1]
-                    conn = sqlite3.connect('db/{}.sqlite'.format(tickr))
-                    print("-- Crawl Comp: {} --".format(tickr))
-                    try:
-                        crawler(tickr)
-                    except Exception as e:
-                        print(e)
-                    conn.close()
-        # # -- 3. Modify data --
-        # elif choice == 3:
-        #     disp_menu_3()
-        #     choice = int(input("Choose function: "))
-        #     print("-----------------------------------------")
-        #     # -- 3-0. Exit program --
-        #     if choice == 0:
-        #         break
-        #     # -- 3-1. Clear table (single) --
-        #     elif choice == 1:
-        #         tickr = str(input("Clear which company? "))
-        #         confirm = str(input("Delete {}? (Y/N) ".format(comp_name)))
-        #         if confirm == 'Y' or confirm == 'y':
-        #             conn = sqlite3.connect('db/{}.sqlite'.format(tickr))
-        #             sqlstr = 'DELETE * from info'
-        #             cursor = conn.execute(sqlstr)
-        #             conn.execute(sqlstr)
-        #             conn.commit()
-        #         print('-- Tables is cleared --')
-        #     # -- 3-2. Clear table (all) --
-        #     elif choice == 2:
-        #         confirm = str(input("Clear all tables? (Y/N) "))
-        #         if confirm == 'Y' or confirm == 'y':
-        #             for row in querylis:
-        #                 tickr = row[0]
-        #                 conn = sqlite3.connect('db/{}.sqlite'.format(tickr))
-        #                 sqlstr = 'DELETE * from info'
-        #                 cursor = conn.execute(sqlstr)
-        #                 conn.execute(sqlstr)
-        #                 conn.commit()
-        #         print('-- Tables are cleared --')
+                pass
+            # -- 4-3. Show all --
+            elif choice == 3:
+                pass
+        # -- 4. Show data --
+        elif choice == 4:
+            Display.show()
+            choice = int(input("Choose function: "))
+            print("-----------------------------------------")
+            # -- 4-0. Exit program --
+            if choice == 0:
+                break
+            # -- 4-1. Show income --
+            elif choice == 1:
+                Show.income()
+            # -- 4-2. Show expense --
+            elif choice == 2:
+                pass
+            # -- 4-3. Show all --
+            elif choice == 3:
+                pass
